e8419602ffdb196cd587b2b5a6a602675a121770
Bryan Gruneberg <hello@bryangruneberg.com>
2024-02-08 11:51:43 -0600

    Updating nuker output

--- drupclean/drupclean_nuke.php
+++ drupclean/drupclean_nuke.php
@@ -4,6 +4,13 @@ include("drupclean_base.php");
 
 $results = locateAndNukeNukeableTables();
 
-echo "Nukeables nuked: ". count($results['list']) . PHP_EOL;
+echo "==================================================================" . PHP_EOL;
+echo "* Nukeable tables are the tables which match the _tmp or _old pattern" . PHP_EOL;
+echo "* and have be automatically nuked by this Drupclean Nuke job." . PHP_EOL;
+echo "==================================================================" . PHP_EOL;
+echo "Nukeables found: ". count($results['list']) . PHP_EOL;
+echo PHP_EOL;
+
+
 echo "Report File: " . $results['destination'] . PHP_EOL;
 echo "Report URL: " . $results['url'] . PHP_EOL;
